f4b6ca93e305fdf6be5cf4ac8ac901d61c11daf4
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-22 16:00:00 -0300

    :lipstick: Create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button, ThemeProvider } from '@material-ui/core';
 import React from 'react';
+import { theme } from './styles/globlal-styles';
 
 function App(): JSX.Element {
   return (
-    <h1>Hello world!</h1>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </ThemeProvider>
   );
 }
 
