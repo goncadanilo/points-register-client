31278bbe8e4a0ee8616b8ef5e472ded5a38e23bf
Danilo Gonçalves <gonca.danilo@gmail.com>
2021-01-22 17:17:11 -0300

    :lipstick: Create App.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
